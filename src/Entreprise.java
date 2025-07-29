648e1ca6229034ab79d8e8de17d1798716dfa2b0
raphaelmakaryan <contact@raphaelmakaryan.fr>
2025-07-29 10:02:43 +0200

    5 — On déménage !

---
+++ src/Entreprise.java
@@ -0,0 +1,19 @@
+public class Entreprise {
+    public static void main(String[] args) {
+        int cartons = 34;
+        int maxCamion = 9;
+        int voyages = 0;
+        while (cartons > 1) {
+            int difference = cartons - maxCamion;
+            if (difference > 0) {
+                cartons = difference;
+                voyages++;
+                System.out.println("La difference est positif, donc le voyage de " + maxCamion + " cartons pour le " + voyages + " eme voyage, il y'a maintenant " + cartons + " cartons");
+            } else {
+                cartons = difference - (cartons - maxCamion);
+                voyages++;
+                System.out.println("La difference est négative, donc le voyage de " + difference + " cartons pour le " + voyages + " eme voyage, il y'a maintenant " + cartons + " cartons");
+            }
+        }
+    }
+}
